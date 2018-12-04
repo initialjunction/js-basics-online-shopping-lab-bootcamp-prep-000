b8b8915a05fc24abedee86287c84db3ce7c09c3c
Joanna Tomassoni <jctomassoni@gmail.com>
2018-12-04 02:42:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,12 +9,21 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(name) {
+  var item = { itemName: name, itemPrice: ((Math.floor(Math.random() * (101 - 1)) + 1))};
+  cart.push(item);
+  return `${name} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length > 0) {
+    for (let i = 0; i < cart.length; i++) {
+      var full = `${itemName} at ${itemPrice}.`
+    }
+    return "In your cart, you have " + full;
+  } else {
+    return "Your shopping cart is empty.";
+  }
 }
 
 function total() {
