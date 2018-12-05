0d3c9262467b10df6ae07989f59f244d3e04675c
Joanna Tomassoni <jctomassoni@gmail.com>
2018-12-05 09:29:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,12 +9,37 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(name) {
+  var item = { itemName: name, itemPrice: ((Math.floor(Math.random() * (101 - 1)) + 1))};
+  cart.push(item);
+  return `${name} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length > 0) {
+    var i = 0;
+    for (let i = 0; i < cart.length; i++) {
+      var full = `${cart.itemName[i]} at ${"$cart.itemPrice[i]"}.`
+    }
+    return "In your cart, you have " + full;
+  } else {
+    return "Your shopping cart is empty.";
+  }
+}
+
+
+function currentLine(line) {
+  if (line.length > 0) {
+    var i = 0;
+    var totalLine = [];
+    for (i = 0; i < line.length; i++) {
+      var lineItem = `${i + 1}. ${line[i]}`;
+      totalLine.push(lineItem);
+    }
+    return "The line is currently: " + totalLine.join(", ");
+  } else {
+    return "The line is currently empty.";
+  }
 }
 
 function total() {
